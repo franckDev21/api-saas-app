8fbda517aab5bcb6a4442c27437c917353389366
franckDev21 <tutodevphp@gmail.com>
2022-11-07 09:20:55 +0100

    deploy api

--- app/Http/Controllers/Api/V1/FileController.php
+++ app/Http/Controllers/Api/V1/FileController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class FileController extends Controller
-{
+{ 
     public function generateBase64(Request $request){
         $request->validate([
             'image' => 'required'
